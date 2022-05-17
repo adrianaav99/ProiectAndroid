ab650e440e687b733247ed68aa9865e0c6d0fc06
Adriana Vladone <adriana.vladone@s.unibuc.ro>
2022-05-17 17:00:27 +0300

    Commit

--- app/src/main/java/com/example/proiectandroid/Camera.kt
+++ app/src/main/java/com/example/proiectandroid/Camera.kt
@@ -1,7 +1,8 @@
 package com.example.proiectandroid
-
-import android.content.ContentResolver
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
@@ -11,15 +12,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.contentValuesOf
 import androidx.core.view.drawToBitmap
 import androidx.databinding.DataBindingUtil
 import com.example.proiectandroid.databinding.ActivityMainBinding
 import com.example.proiectandroid.databinding.FragmentCameraBinding
+import com.google.android.material.internal.ContextUtils
+import com.google.android.material.internal.ContextUtils.getActivity
 import java.io.File
 import java.io.OutputStream
+import java.security.AccessController.getContext
 import java.util.*
 
 class Camera : Fragment() {
@@ -30,10 +36,24 @@ class Camera : Fragment() {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentCameraBinding>(inflater,
             R.layout.fragment_camera, container, false)
-        binding.buttonPg2.setOnClickListener {
-            val i= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(i, 101)
+
+        binding.buttonPg2.isEnabled = false
+        var context = getContext()
+        if(context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.CAMERA) } != PackageManager.PERMISSION_GRANTED){
+            activity?.let { ActivityCompat.requestPermissions(it,
+                arrayOf(Manifest.permission.CAMERA), 111) }
+        }else{
+            binding.buttonPg2.isEnabled = true
+            binding.buttonPg2.setOnClickListener {
+                val i= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(i, 101)
+            }
         }
+
+        //binding.buttonPg2.setOnClickListener {
+        //    val i= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        //    startActivityForResult(i, 101)
+        //}
 
         binding.button2Pg2.setOnClickListener (View.OnClickListener {
             val imageView: ImageView = requireView().findViewById(R.id.imagine_camera)
@@ -45,6 +65,19 @@ class Camera : Fragment() {
         return binding.root
     }
 
+
+    @Suppress("DEPRECATION")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            var buton: Button = requireView().findViewById(R.id.button_pg_2)
+            buton.isEnabled = true
+        }
+    }
     // Creating our Share Intent
 
     @Suppress("DEPRECATION")
@@ -76,12 +109,13 @@ class Camera : Fragment() {
                 Objects.requireNonNull<OutputStream?>(fos)
                 Toast.makeText(activity,"Imagine salvata", Toast.LENGTH_SHORT).show()
 
-                val shareIntent: Intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_STREAM, imageUri)
-                    type = "image/jpeg"
-                }
-                startActivity(Intent.createChooser(shareIntent, null))
+                //
+                //val shareIntent: Intent = Intent().apply {
+                //    action = Intent.ACTION_SEND
+                //    putExtra(Intent.EXTRA_STREAM, imageUri)
+                //    type = "image/jpeg"
+                //}
+                //startActivity(Intent.createChooser(shareIntent, null))
             }
         }
         catch (e: Exception){
